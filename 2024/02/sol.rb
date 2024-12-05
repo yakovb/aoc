776ea6f5e58f12d6c69717deeab28ff3dece6e22
yakovb <y@kov.com>
2024-12-05 23:05:28 +1100

    should sort each line lazily, not when reading the file

--- 2024/02/sol.rb
+++ 2024/02/sol.rb
@@ -2,15 +2,20 @@ data = File
   .readlines(ARGV[0])
   .map { |s| s.scan(/\d+/).map &:to_i }
 
-data.each do |r|
-  unless r == r.sort
-    r.reverse!
-  end
+def norm(arr)
+  arr == arr.sort ? arr : arr.reverse
 end
 
 def safe(arr)
-  arr.each_cons(2).all? { |a,b| (b-a).between?(1, 3) }
+  norm(arr).each_cons(2).all? { |a,b| (b-a).between?(1, 3) }
 end
 
 # one
 p data.map { |r| safe(r) }.count true
+
+# two
+p data.map { |r| 
+  (0...r.length).map { |i| a = r.drop(0); a.delete_at(i); a }
+    .map { |perm| safe(perm) }
+    .any?
+}.count true
