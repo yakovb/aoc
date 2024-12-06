f12ccb6909d41e8f8c9f5f446a5c32b0a71f757f
yakovb <y@kov.com>
2024-12-06 23:06:43 +1100

    refactor, day 2 is still wrong

--- 2024/04/sol.rb
+++ 2024/04/sol.rb
@@ -4,14 +4,16 @@ $grid = File
     .readlines(ARGV[0])
     .map { |line| line.chomp.chars }
 
-rmax = $grid.size
-cmax = $grid.first.size
+$rmax = $grid.size
+$cmax = $grid.first.size
 
-def xmas(idxs)
-    return 0 if idxs.flatten.any? { |i| i < 0 }
+
+def word(idxs, str)
+    return 0 if idxs.flatten.any? { |i| i < 0 || i >= $rmax }
     idxs
         .map { |r,c| $grid.dig(r,c) }
-        .join == "XMAS" ? 1 : 0
+        .join
+        .start_with?(str) ? 1 : 0
 end
 
 def fwd(i)
@@ -26,48 +28,57 @@ end
 
 ### ### ###
 def right(r,c)
-    xmas( (const(r)).zip( fwd(c) ) )
+    const(r).zip( fwd(c) )
 end
 
 def left(r,c)
-    xmas( const(r).zip( bwd(c) ) )
+    const(r).zip( bwd(c) )
 end
 
 def up(r,c)
-    xmas( bwd(r).zip( const(c) ) )
+    bwd(r).zip( const(c) )
 end
 
 def down(r,c)
-    xmas( fwd(r).zip( const(c) ) )
+    fwd(r).zip( const(c) )
 end
 
 def upright(r,c)
-    xmas( bwd(r).zip( fwd(c) ) )
+    bwd(r).zip( fwd(c) )
 end
 
 def upleft(r,c)
-    xmas( bwd(r).zip( bwd(c) ) )
+    bwd(r).zip( bwd(c) )
 end
 
 def downright(r,c)
-    xmas( fwd(r).zip( fwd(c) ) )
+    fwd(r).zip( fwd(c) )
 end
 
 def downleft(r,c)
-    xmas( fwd(r).zip( bwd(c) ) )
+    fwd(r).zip( bwd(c) )
 end
 ### ### ###
 
-def check(r,c)
-    right(r,c) + left(r,c) + up(r,c) + down(r,c) +
-        upright(r,c) + upleft(r,c) + downright(r,c) + downleft(r,c)
+def xmas(r,c)
+    [right(r,c), left(r,c), up(r,c), down(r,c),
+        upright(r,c), upleft(r,c), downright(r,c), downleft(r,c)]
+        .sum { |idx| word(idx, "XMAS") }
+end
+
+def mas(r,c)
+    [upright(r,c), upleft(r,c), downright(r,c), downleft(r,c)]
+        .sum { |idx| word(idx, "MAS") }
 end
 
 nxmas = 0
-(0...rmax).each do |row|
-    (0...cmax).each do |col|
-        nxmas += check(row, col)
+nmas = 0
+(0...$rmax).each do |row|
+    (0...$cmax).each do |col|
+        nxmas += xmas(row, col)
+        nmas += mas(row, col)
     end
 end
 
 p nxmas
+p nmas
