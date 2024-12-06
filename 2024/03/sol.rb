9c6541e273922b32e15fdb920940dfeb79270c24
yakovb <y@kov.com>
2024-12-06 14:40:37 +1100

    refactor day 1, do day 2

--- 2024/03/sol.rb
+++ 2024/03/sol.rb
@@ -1,10 +1,18 @@
 #!/usr/bin/env ruby
 
-p File
-    .readlines(ARGV[0])
-    .map { |line| line.scan(/mul\(\d{1,3},\d{1,3}\)/) }
-    .map { |mul| 
-        mul.map { |s| s.scan(/\d{1,3}/).map &:to_i } }
-    .map { |line| 
-        line.sum { |pair| a,b = pair; a*b } }
-    .sum
+input = File.read(ARGV[0])
+
+def mul(str)
+    str
+        .scan( /mul\((\d{1,3}),(\d{1,3})\)/ )
+        .sum { |a,b| a.to_i * b.to_i }
+end
+
+# one
+p mul(input)
+
+# two
+p input
+    .split(/do\(\)/)
+    .map { |part| part.split(/don't\(\)/).first }
+    .sum { |s| mul(s) }
